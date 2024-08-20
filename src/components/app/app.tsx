8d76dc034ae73a8a1a388828d0c7fe0db05f5617
Marina Balashova <deine.kleine.liebe@gmail.com>
2024-08-20 07:24:37 +0300

    initial commit

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,46 @@
-import { ConstructorPage } from '@pages';
+import {
+  ConstructorPage,
+  Feed,
+  ForgotPassword,
+  Login,
+  NotFound404,
+  Profile,
+  ProfileOrders,
+  Register,
+  ResetPassword
+} from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
+import {
+  Routes,
+  Route,
+  Outlet,
+  useLocation,
+  useNavigate
+} from 'react-router-dom';
 
 import { AppHeader } from '@components';
 
-const App = () => (
-  <div className={styles.app}>
-    <AppHeader />
-    <ConstructorPage />
-  </div>
-);
+const App = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const background = location.state?.background;
+
+  return (
+    <div className={styles.app}>
+      <AppHeader />
+      <Routes location={background || location}>
+        <Route path='/' element={<ConstructorPage />} />
+        <Route path='/feed' element={<Feed />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/forgot-password' element={<ForgotPassword />} />
+        <Route path='/reset-password' element={<ResetPassword />} />
+        <Route path='/profile/orders' element={<Profile />} />
+        <Route path='*' element={<NotFound404 />} />
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
