2b3153610ac8632ca43c1296057fd58031cbf002
Dasha <dia.emelianova@yandex.ru>
2024-08-21 22:43:26 +0300

    исправлены замечения после ревью

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,9 @@ export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      
     },
+    baseUrl: 'http://localhost:4000/#/',
+
   },
 });
--- cypress/e2e/constructor.cy.tsx
+++ cypress/e2e/constructor.cy.tsx
@@ -1,39 +1,45 @@
+const constructorElementTextSelector = 'div.constructor-element span.constructor-element__text'
+const ingredientBunSelector = '[data-cy=bun-ingredients]'
+const ingredientMainSelector = '[data-cy=main-ingredients]'
+const ingredientSauceSelector = '[data-cy=sauce-ingredients]'
+const modalInfoSelector = '[data-cy=modal-info]'
+const closeX = '[data-cy=closeX]'
 
 
 describe('Проверка доступности приложения', () => {
     beforeEach(() => {
         cy.intercept('GET', '/api/ingredients', { fixture: 'ingredients.json' }).as ('getIngredients');
         cy.intercept('GET', '/api/auth/user', { fixture: 'user.json' }).as ('getAuth')
-        cy.visit('http://localhost:4000')
-        cy.get('[data-cy=bun-ingredients]').as ('bunIngredients');
-        cy.get('[data-cy=main-ingredients]').as ('mainIngredients');
-        cy.get('[data-cy=sauce-ingredients]').as ('sauceIngredients');
+        cy.visit('/')
+        cy.get(ingredientBunSelector).as ('bunIngredients');
+        cy.get(ingredientMainSelector).as ('mainIngredients');
+        cy.get(ingredientSauceSelector).as ('sauceIngredients');
     })
 
     it('Проверка добавления ингредиентов в конструктор', () => {
         cy.get('@bunIngredients').contains('Добавить').click();
-        cy.get('div.constructor-element span.constructor-element__text').contains('Краторная булка N-200i (верх)')
-        cy.get('div.constructor-element span.constructor-element__text').contains('Краторная булка N-200i (низ)')
+        cy.get(constructorElementTextSelector).contains('Краторная булка N-200i (верх)')
+        cy.get(constructorElementTextSelector).contains('Краторная булка N-200i (низ)')
         cy.get('@mainIngredients').contains('Добавить').click()
-        cy.get('div.constructor-element span.constructor-element__text').contains('Биокотлета из марсианской Магнолии')
+        cy.get(constructorElementTextSelector).contains('Биокотлета из марсианской Магнолии')
         cy.get('@sauceIngredients').contains('Добавить').click()
-        cy.get('div.constructor-element span.constructor-element__text').contains('Соус Spicy-X')
+        cy.get(constructorElementTextSelector).contains('Соус Spicy-X')
     })
 })
 
 describe('Проверка работы модальных окон', () => {
     beforeEach(() => {
         cy.intercept('GET', '/api/ingredients', { fixture: 'ingredients.json' }).as ('getIngredients')
-        cy.visit('http://localhost:4000')
+        cy.visit('/')
     })
 
     it('Модального окна нет', () => {
-        cy.get('[data-cy=modal-info]').should('not.exist');
+        cy.get(modalInfoSelector).should('not.exist');
       });
 
     it('Модальное окно открывается', () => {
         cy.contains('Соус Spicy-X').click()
-        cy.get('[data-cy=modal-info]').should('exist');
+        cy.get(modalInfoSelector).should('exist');
       cy.get('li').children('p').contains('Калории, ккал').next('p').contains('30');
       cy.get('li').children('p').contains('Белки, г').next('p').contains('30');
       cy.get('li').children('p').contains('Жиры, г').next('p').contains('20');   
@@ -42,17 +48,17 @@ describe('Проверка работы модальных окон', () => {
 
     it('Модальное окно закрывается по Х', function() {
         cy.contains('Соус Spicy-X').click();
-        cy.get('[data-cy=modal-info]').should('exist'); 
-        cy.get('[data-cy=closeX]').should('exist');
-        cy.get('[data-cy=closeX]').click();
-        cy.get('[data-cy=modal-info]').should('not.exist'); 
+        cy.get(modalInfoSelector).should('exist'); 
+        cy.get(closeX).should('exist');
+        cy.get(closeX).click();
+        cy.get(modalInfoSelector).should('not.exist'); 
       });
 
       it('Модальное окно закрывается по клику на оверлее', function() {
         cy.contains('Соус Spicy-X').click();
-        cy.get('[data-cy=modal-info]').should('exist'); 
+        cy.get(modalInfoSelector).should('exist'); 
         cy.get('[data-cy=closeOverlay]').click('bottomLeft', {force: true});      
-        cy.get('[data-cy=modal-info]').should('not.exist');
+        cy.get(modalInfoSelector).should('not.exist');
       });  
 })
 
@@ -64,14 +70,14 @@ describe('Проверка оформления заказа', function() {
       cy.intercept('POST', '/api/orders', { fixture: 'order.json' });
 
 
-      cy.visit('http://localhost:4000'); 
+      cy.visit('/'); 
 
       cy.setCookie('accessToken', 'accessToken');
         window.localStorage.setItem('refreshToken', 'refreshToken');    
 
-      cy.get('[data-cy=bun-ingredients]').as ('bunIngredients');
-      cy.get('[data-cy=main-ingredients]').as ('mainIngredients');
-      cy.get('[data-cy=sauce-ingredients]').as ('sauceIngredients');
+      cy.get(ingredientBunSelector).as ('bunIngredients');
+      cy.get(ingredientMainSelector).as ('mainIngredients');
+      cy.get(ingredientSauceSelector).as ('sauceIngredients');
     });
 
     afterEach(() => {
@@ -81,16 +87,16 @@ describe('Проверка оформления заказа', function() {
 
     it('Проверка оформления заказа', function() {
       cy.get('@bunIngredients').contains('Добавить').click();      
-      cy.get('div.constructor-element span.constructor-element__text').contains('Краторная булка N-200i (верх)');
-      cy.get('div.constructor-element span.constructor-element__text').contains('Краторная булка N-200i (низ)');
+      cy.get(constructorElementTextSelector).contains('Краторная булка N-200i (верх)');
+      cy.get(constructorElementTextSelector).contains('Краторная булка N-200i (низ)');
       cy.get('@sauceIngredients').contains('Добавить').click();      
-      cy.get('div.constructor-element span.constructor-element__text').contains('Соус Spicy-X');
+      cy.get(constructorElementTextSelector).contains('Соус Spicy-X');
       
       cy.get('[data-cy=onOrder]').click();
-      cy.get('[data-cy=modal-info]').should('exist');
+      cy.get(modalInfoSelector).should('exist');
       cy.get('#modals').find('h2').contains(77777);
-      cy.get('[data-cy=closeX]').click();
-      cy.get('[data-cy=modal-info]').should('not.exist');
+      cy.get(closeX).click();
+      cy.get(modalInfoSelector).should('not.exist');
       cy.get('[data-cy=topBun]').contains('Выберите булки');
       cy.get('[data-cy=bottomBun]').contains('Выберите булки');
       cy.get('[data-cy=inside]').contains('Выберите начинку');
