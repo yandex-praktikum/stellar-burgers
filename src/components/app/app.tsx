3ab47941ef1fe5ee9982756d6c66f11210acb25f
deine.kleine.liebe@gmail.com <ms.balashova.2001@gmail.com>
2024-08-23 20:51:00 +0300

    Add new routes

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -19,7 +19,7 @@ import {
   useNavigate
 } from 'react-router-dom';
 
-import { AppHeader } from '@components';
+import { AppHeader, IngredientDetails, Modal, OrderInfo } from '@components';
 
 const App = () => {
   const navigate = useNavigate();
@@ -30,14 +30,53 @@ const App = () => {
     <div className={styles.app}>
       <AppHeader />
       <Routes location={background || location}>
+        <Route path='*' element={<NotFound404 />} />
         <Route path='/' element={<ConstructorPage />} />
         <Route path='/feed' element={<Feed />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/forgot-password' element={<ForgotPassword />} />
-        <Route path='/reset-password' element={<ResetPassword />} />
-        <Route path='/profile/orders' element={<Profile />} />
-        <Route path='*' element={<NotFound404 />} />
+        <Route path='/login' element={<ProtectedRoute><Login /></ProtectedRoute>} />
+        <Route path='/register' element={<ProtectedRoute><Register /></ProtectedRoute>} />
+        <Route path='/forgot-password' element={<ProtectedRoute><ForgotPassword /></ProtectedRoute>} />
+        <Route path='/reset-password' element={<ProtectedRoute><ResetPassword /></ProtectedRoute>} />
+        <Route path='/profile/orders' element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+        <Route
+          path='/feed/:number'
+          element={
+            <Modal
+              title={''}
+              onClose={function (): void {
+                throw new Error('Function not implemented.');
+              }}
+            >
+              <OrderInfo />
+            </Modal>
+          }
+        />
+        <Route
+          path='/ingredients/:id'
+          element={
+            <Modal
+              title={''}
+              onClose={function (): void {
+                throw new Error('Function not implemented.');
+              }}
+            >
+              <IngredientDetails />
+            </Modal>
+          }
+        />
+        <Route
+          path='/profile/orders/:number'
+          element={
+            <Modal
+              title={''}
+              onClose={function (): void {
+                throw new Error('Function not implemented.');
+              }}
+            >
+              <OrderInfo />
+            </Modal>
+          }
+        />
       </Routes>
     </div>
   );
