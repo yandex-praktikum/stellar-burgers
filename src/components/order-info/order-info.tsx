66734dd4a2d773aef098287fc6606bc2ecca4ea5
Ksenia Semenova <palm87@gmail.com>
2024-08-31 09:38:32 +0200

    Модалка с заказом сделана

--- src/components/order-info/order-info.tsx
+++ src/components/order-info/order-info.tsx
@@ -1,23 +1,26 @@
-import { FC, useMemo } from 'react';
+import { FC, useEffect, useMemo, useState } from 'react';
 import { Preloader } from '../ui/preloader';
 import { OrderInfoUI } from '../ui/order-info';
-import { TIngredient } from '@utils-types';
+import { TIngredient, TOrder } from '@utils-types';
 import { useSelector } from '../../services/store';
 import { getIngredients } from '../../services/ingredientsSlice';
+import { useParams } from 'react-router-dom';
+import { getOrderByNumberApi } from '@api';
 
 export const OrderInfo: FC = () => {
-  /** TODO: взять переменные orderData и ingredients из стора */
-  const orderData = {
-    createdAt: '',
-    ingredients: [],
-    _id: '',
-    status: '',
-    name: '',
-    updatedAt: 'string',
-    number: 0
-  };
+  const [orderData, setOrderData] = useState<TOrder | null>(null);
+
+  const id = Number(useParams().number);
 
   const ingredients: TIngredient[] = useSelector(getIngredients);
+
+  useEffect(() => {
+    if (id) {
+      getOrderByNumberApi(id).then((data) => {
+        setOrderData(data.orders[0]);
+      });
+    }
+  }, [id]);
 
   /* Готовим данные для отображения */
   const orderInfo = useMemo(() => {
--- src/pages/feed/feed.tsx
+++ src/pages/feed/feed.tsx
@@ -1,3 +1,38 @@
+// import { Preloader } from '@ui';
+// import { FeedUI } from '@ui-pages';
+// import { TOrder } from '@utils-types';
+// import { FC, useEffect, useCallback } from 'react';
+// import {
+//   fetchFeeds,
+//   getLoadingFeed,
+//   getOrders
+// } from '../../services/feedSlice';
+// import { useDispatch, useSelector } from '../../services/store';
+
+// export const Feed: FC = () => {
+//   const dispatch = useDispatch();
+//   const orders: TOrder[] = useSelector(getOrders);
+//   const isLoading = useSelector(getLoadingFeed);
+
+//   useEffect(() => {
+//     dispatch(fetchFeeds());
+//   }, [dispatch]);
+
+//   const handleGetFeed = useCallback(() => {
+//     dispatch(fetchFeeds());
+//   }, [dispatch]);
+
+//   if (isLoading) {
+//     return <Preloader />;
+//   }
+
+//   return (
+//     <>
+//       <FeedUI orders={orders} handleGetFeeds={handleGetFeed} />
+//     </>
+//   );
+// };
+
 import { Preloader } from '@ui';
 import { FeedUI } from '@ui-pages';
 import { TOrder } from '@utils-types';
@@ -14,22 +49,17 @@ export const Feed: FC = () => {
   const orders: TOrder[] = useSelector(getOrders);
   const isLoading = useSelector(getLoadingFeed);
 
-  useEffect(() => {
-    console.log('Fetching feeds...');
+  const loadFeeds = useCallback(() => {
     dispatch(fetchFeeds());
   }, [dispatch]);
 
-  const handleGetFeed = useCallback(() => {
-    dispatch(fetchFeeds());
-  }, [dispatch]);
+  useEffect(() => {
+    loadFeeds();
+  }, [loadFeeds]);
 
   if (isLoading) {
     return <Preloader />;
   }
 
-  return (
-    <>
-      <FeedUI orders={orders} handleGetFeeds={handleGetFeed} />
-    </>
-  );
+  return <FeedUI orders={orders} handleGetFeeds={loadFeeds} />;
 };
