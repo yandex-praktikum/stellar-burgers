eca20a90ec97fcf110091c6f06af187549170697
dexyo1337 <dexyo1337@yandex.ru>
2024-10-01 11:00:21 +0300

    fix cypress tests

--- cypress/e2e/constructor.cy.ts
+++ cypress/e2e/constructor.cy.ts
@@ -1,7 +1,5 @@
-// import { URL } from '../../src/utils/burger-api';
-// const firstBun = '[data-cy="ingredient-link-643d69a5c3f7b9001cfa093c"]';
-const firstBun = '[data-cy="ingredient-link-item"] button[type=button]';
-const firstFilling = '[data-cy="ingredient-link-643d69a5c3f7b9001cfa0941"]';
+const ingredient = '[data-cy="ingredient-link-item"]';
+// const firstFilling = '[data-cy="ingredient-link-643d69a5c3f7b9001cfa0941"]';
 
 describe('burger constructor, create order', () => {
   beforeEach(() => {
@@ -30,20 +28,13 @@ describe('burger constructor, create order', () => {
 
   describe('constructor', () => {
     it('get ingredients', () => {
-      // cy.getCookie('accessToken').should('exist');
-      cy.get('[data-cy="ingredient-link-item"]')
-        .should('exist')
-        .and('be.visible');
+      cy.get(ingredient).first().next().click();
       cy.get('[data-cy="constructor"]').as('constructor');
-      cy.get(firstBun).should('exist');
-      // Кликнуть по кнопке "Добавить" первого ингредиента
-      cy.get(firstBun).click();
-
       cy.get('@constructor').should('contain', 'Краторная булка N-200i');
     });
 
     it('open and close ingredient modal', () => {
-      cy.get(firstBun).click();
+      cy.get(ingredient).first().click();
       cy.get('[data-cy="modal"]').as('modal');
       cy.get('@modal').should('exist');
       cy.get('[data-cy="close-button"]').click();
@@ -51,25 +42,28 @@ describe('burger constructor, create order', () => {
     });
 
     it('open and close ingredient modal by overlay', () => {
-      cy.get(firstBun).click();
+      cy.get(ingredient).first().click();
       cy.get('[data-cy="modal"]').as('modal');
       cy.get('@modal').should('exist');
       cy.get('[data-cy="modal-overlay"').click('top', { force: true });
       cy.get('@modal').should('not.exist');
     });
-  });
 
-  describe('order', () => {
     it('create order', () => {
-      // cy.getCookie('accessToken').should('exist');
       cy.get('[data-cy="constructor"]').as('constructor');
-      cy.get(firstBun).next('button').click();
-      cy.get(firstFilling).next('button').click();
+      cy.get(ingredient).first().next().click();
+      cy.get(ingredient).eq(2).next().click();
       cy.get('[data-cy="getOrder-button"]').click();
       cy.wait('@createOrder');
       cy.get('[data-cy="modal"]').as('modal');
       cy.get('@modal').should('exist');
       cy.get('@modal').should('contain', '555');
+
+      cy.get('@modal').should('exist');
+      cy.get('[data-cy="modal-overlay"').click('top', { force: true });
+      cy.get('@modal').should('not.exist');
+
+      cy.get('@constructor').should('contain', '');
     });
   });
 });
