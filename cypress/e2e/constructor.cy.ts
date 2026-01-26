f993b83c1dd72f9f3d01dd0b43d399676ec6c784
Pasha Lobach <Pashensia@gmail.com>
2026-01-26 16:30:06 +0100

    refactor: cypress fix all BeforeEach for mock

--- cypress/e2e/constructor.cy.ts
+++ cypress/e2e/constructor.cy.ts
@@ -1,62 +1,78 @@
 describe('проверяем доступность приложения', function () {
-  it('добавляет ингредиент в конструктор', () => {
+  beforeEach(function () {
+    // Настройка перехвата запросов во всех тестах - обязательное требование
+    cy.intercept('GET', '**/ingredients', { fixture: 'ingredients.json' }).as(
+      'ingredients'
+    );
+    cy.viewport(1300, 800);
     cy.visit('http://localhost:4000');
-
+  });
+  it('добавляет ингредиент в конструктор', () => {
     // проверка конструктора на ингридиенты
+    cy.wait('@ingredients');
 
     cy.get("[data-cy='constructor'] li").should('have.length', 0);
 
-    cy.get("[data-cy='643d69a5c3f7b9001cfa093e']").find('button').click();
-    cy.get("[data-cy='643d69a5c3f7b9001cfa0941']").find('button').click();
+    cy.get("[data-cy='01']").find('button').click();
+    cy.get("[data-cy='02']").find('button').click();
 
-    cy.get("[data-cy='constructor'] li").should('have.length', 2);
+    cy.get("[data-cy='constructor'] li").should('have.length', 1);
     // --------------------------------------
 
     // Проверка контркутора на булки
 
-    cy.get("[data-cy='643d69a5c3f7b9001cfa093d']").find('button').click();
+    cy.get("[data-cy='01']").find('button').click();
     cy.get("[data-cy='bun']").should('have.length', 2);
   });
 });
 
 describe('проверяем модалку', function () {
-  it('закрытие модалки на крестик', () => {
+  beforeEach(function () {
+    // Настройка перехвата запросов во всех тестах - обязательное требование
+    cy.intercept('GET', '**/ingredients', { fixture: 'ingredients.json' }).as(
+      'ingredients'
+    );
+    cy.viewport(1300, 800);
     cy.visit('http://localhost:4000');
-
-    // cy.get("[data-cy='643d69a5c3f7b9001cfa0941']").click();
+  });
+  it('закрытие модалки на крестик', () => {
+    cy.wait('@ingredients');
+    cy.get("[data-cy='02']").click();
 
     // вижу ваш комментарий по поводу этого компонента ибо он не открываеться у вас но я не вижу причины почему он не открываеться ибо у меня все работает исправно открываеться модалка карточки и потом закрываеться, в компоненте бургер конструктор указана дата элементам и она береться с ингридиента его айди
-
-    cy.contains('Краторная булка N-200i').click();
 
     // тут я нажимаю на элемент по содержимому надеюсь оно будет работать ибо у меня работает )
 
     cy.get("[data-cy='modal']").should('be.visible');
+    cy.get("[data-cy='modal']").contains('Начинка для бургера');
     cy.get("[data-cy='modal']").find('button').click();
     cy.get("[data-cy='modal']").should('not.exist');
   });
 });
 
 describe('проверка создания заказа', () => {
-  afterEach(() => {
-    cy.clearLocalStorage();
-    cy.clearCookies();
-  });
-
-  it('создаем заказ с моковыми данными', () => {
+  beforeEach(function () {
+    // Настройка перехвата запросов во всех тестах - обязательное требование
     cy.intercept('GET', '**/ingredients', { fixture: 'ingredients.json' }).as(
       'ingredients'
     );
     cy.intercept('GET', '**/auth/user', { fixture: 'user.json' }).as('user');
     cy.intercept('POST', '**/orders', { fixture: 'order.json' }).as('order');
-
+    cy.viewport(1300, 800);
     cy.visit('http://localhost:4000', {
       onBeforeLoad(win) {
         win.localStorage.setItem('refreshToken', 'test-refresh-token');
         win.document.cookie = 'accessToken=test-access-token; path=/';
       }
     });
+  });
 
+  afterEach(() => {
+    cy.clearLocalStorage();
+    cy.clearCookies();
+  });
+
+  it('создаем заказ с моковыми данными', () => {
     cy.wait('@ingredients');
     cy.wait('@user');
 
