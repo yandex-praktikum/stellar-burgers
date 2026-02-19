dd90abceeadce93b2d7f2d5a753a3b1615b9e316
Andrey Kruglov <kruglov.it@gmail.com>
2026-02-19 10:50:42 +0300

    fix: library and other issues

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -3,12 +3,32 @@ import '../../index.css';
 import styles from './app.module.css';
 
 import { AppHeader } from '@components';
+import { Preloader } from '@ui';
 
-const App = () => (
-  <div className={styles.app}>
-    <AppHeader />
-    <ConstructorPage />
-  </div>
-);
+const App = () => {
+  /** TODO: взять переменные из стора */
+  const isIngredientsLoading = false;
+  const ingredients = [];
+  const error = 'Byaka';
+
+  return (
+    <div className={styles.app}>
+      <AppHeader />
+      {isIngredientsLoading ? (
+        <Preloader />
+      ) : error ? (
+        <div className={`${styles.error} text text_type_main-medium pt-4`}>
+          {error}
+        </div>
+      ) : ingredients.length > 0 ? (
+        <ConstructorPage />
+      ) : (
+        <div className={`${styles.title} text text_type_main-medium pt-4`}>
+          Нет игредиентов
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default App;
